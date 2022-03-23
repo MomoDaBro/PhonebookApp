af106737aa4d1ccc9815431d0857a7c5d0b2c2e0
momoqais <momofqais@gmail.com>
2022-03-23 10:00:19 -0500

    Finished tidying up the phonebook app but i need to add deletion and function with 2 parameters, otherwise everything works.

--- src/Main.java
+++ src/Main.java
@@ -9,58 +9,75 @@ public class Main {
 
         List<Person> phonebook = new ArrayList<>();
 
-        Phonebook phonebook1 = new Phonebook();
-
-
-
-        Person p1 = new Person("Bido", "915-526-4649", "moving water ln", 18);
-        Person p2 = new Person("Imaad", "123456789", "im a bad player rd", 1);
-        Person p3 = new Person("Omar", "987654321", "im an immortal player ct", 20);
+        Person p1 = new Person("PersonA", "12345", "address1", 15);
+        Person p2 = new Person("PersonB", "67890", "address2", 25);
+        Person p3 = new Person("PersonC", "98765", "address3", 20);
 
         phonebook.add(p1);
         phonebook.add(p2);
         phonebook.add(p3);
 
-        phonebook1.addPerson(p1);
-        phonebook1.addPerson(p2);
-        phonebook1.addPerson(p3);
-
-        System.out.println(phonebook1.searchByName("Bido"));
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Would you like to search for a person in the phonebook?");
-        String answer = scanner.nextLine();
-
-        if (answer.equals("yes")) {
-            System.out.println("Press 1 for name, 2 for number, 3 for address, 4 for age?");
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please select what you would like to do within the phonebook");
+            System.out.println("Press 1 to add a person, 2 to delete someone, 3 to search for someone, 4 to print everyone in the phonebook, and 5 to exit");
             int decision = Integer.parseInt(scanner.nextLine());
             if (decision == 1) {
-                System.out.println("What is their name?");
-                String name = scanner.nextLine();
-                searchByName(phonebook, name);
+                addPerson(phonebook, scanner);
             } else if (decision == 2) {
-                System.out.println("What is their number?");
-                String number = scanner.nextLine();
-                searchByNumber(phonebook, number);
             } else if (decision == 3) {
-                System.out.println("What is their address?");
-                String address = scanner.nextLine();
-                searchByAddress(phonebook, address);
+                System.out.println("Press 1 for name, 2 for number, 3 for address, 4 for age");
+                int decision1int = Integer.parseInt(scanner.nextLine());
+                if (decision1int == 1) {
+                    System.out.println("What is their name?");
+                    String name = scanner.nextLine();
+                    searchByName(phonebook, name);
+                } else if (decision1int == 2) {
+                    System.out.println("What is their number?");
+                    String number = scanner.nextLine();
+                    searchByNumber(phonebook, number);
+                } else if (decision1int == 3) {
+                    System.out.println("What is their address?");
+                    String address = scanner.nextLine();
+                    searchByAddress(phonebook, address);
+                } else if (decision1int == 4) {
+                    System.out.println("What is their age?");
+                    int age = scanner.nextInt();
+                    searchByAge(phonebook, age);
+                }
             } else if (decision == 4) {
-                System.out.println("What is their age?");
-                int age = scanner.nextInt();
-                searchByAge(phonebook, age);
+                printPeople(phonebook);
+            } else if (decision == 5) {
+                System.out.println("Thank you for using the phonebook, goodbye.");
+                break;
             }
         }
     }
+    private static void addPerson(List<Person> phonebook, Scanner scanner) {
+        System.out.println("What is their name?");
+        String name = scanner.nextLine();
+        System.out.println("What is their number?");
+        String number = scanner.nextLine();
+        System.out.println("What is their address?");
+        String address = scanner.nextLine();
+        System.out.println("What is their age?");
+        int age = scanner.nextInt();
+        Person newPerson = new Person(name, number, address, age);
+        phonebook.add(newPerson);
+        System.out.println("done");
+    }
 
+    private static void printPeople(List<Person> phonebook) {
+        for (Person p : phonebook) {
+            System.out.println(p);
+        }
+    }
 
     private static void searchByAge(List<Person> phonebook, int age) {
         for (Person p : phonebook) {
             if (p.age == age) {
                 System.out.println(p);
             }
-
         }
     }
 
@@ -86,32 +103,5 @@ public class Main {
                 System.out.println(p);
             }
         }
-    }
-
-//        if (answer.equals("yes")) {
-//            addBerson(phonebook, scanner);
-//        }
-
-//        brintBeoble(phonebook);
-
-
-    private static void brintBeoble(List<Person> phonebook) {
-        for (Person kalb : phonebook) {
-            System.out.println(kalb);
-        }
-    }
-
-    private static void addBerson(List<Person> phonebook, Scanner scanner) {
-        System.out.println("What is their name?");
-        String name = scanner.nextLine();
-        System.out.println("What is their number?");
-        String number = scanner.nextLine();
-        System.out.println("What is their address?");
-        String address = scanner.nextLine();
-        System.out.println("What is their age?");
-        int age = scanner.nextInt();
-        Person newPerson = new Person(name, number, address, age);
-        phonebook.add(newPerson);
-        System.out.println("done");
     }
 }
