fc00b8eca258d2e1c9eab174522490b3f047e7bf
MomoDaBro <Momofqais@gmail.com>
2022-04-21 04:46:53 -0500

    -Completed the phonebook project and ready to be sent to CollegeBoard

--- src/Main.java
+++ src/Main.java
@@ -21,54 +21,23 @@ public class Main {
         while (true) {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Please select what you would like to do within the phonebook");
-            System.out.println("Press 1 to add a person, 2 to update a contact, 3 to search for someone, 4 to print everyone in the phonebook, and 5 to exit");
+            System.out.println("Press 1 to add a person, 2 to search for someone, 3 to print those of legal drinking age, 4 to print those of legal driving age, 5 to print everyone in the phonebook, and 6 to exit");
             int decision = Integer.parseInt(scanner.nextLine());
             if (decision == 1) {
                 addPerson(phonebook, scanner);
             } else if (decision == 2) {
-                updateContact(phonebook, scanner);
-            } else if (decision == 3) {
                 System.out.println(findPeople(phonebook, scanner));
+            } else if (decision == 3) {
+                System.out.println(searchByAgeGreaterThan(phonebook, 21));
             } else if (decision == 4) {
-                printPeople(phonebook);
+                System.out.println(searchByAgeGreaterThan(phonebook, 16));
             } else if (decision == 5) {
+                printPeople(phonebook);
+            } else if (decision == 6) {
                 System.out.println("Thank you for using the phonebook, goodbye.");
                 break;
             }
         }
-    }
-
-    private static void updateContact(List<Person> phonebook, Scanner scanner) {
-        List<Person> filteredList = findPeople(phonebook, scanner);
-        int selection = 0;
-        if (filteredList.size() != 1) {
-            System.out.println("Please pick a person from 0-" + (filteredList.size() - 1));
-            selection = Integer.parseInt(scanner.nextLine());
-        }
-        Person update = filteredList.get(selection);
-        while (true) {
-            System.out.println("Choose an entry to update by Press 1 for name, 2 for number, 3 for address, 4 for age");
-            int decision1int = Integer.parseInt(scanner.nextLine());
-            if (decision1int == 1) {
-                System.out.println("What is the new name?");
-                update.name = scanner.nextLine();
-                break;
-            } else if (decision1int == 2) {
-                System.out.println("What is the new number?");
-                update.number = scanner.nextLine();
-                break;
-            } else if (decision1int == 3) {
-                System.out.println("What is the new address?");
-                update.address = scanner.nextLine();
-                break;
-            } else if (decision1int == 4) {
-                System.out.println("What is the new age?");
-                update.age = scanner.nextInt();
-                break;
-            }
-            System.out.println("Invalid Option, please enter a correct option.");
-        }
-        System.out.println("Done");
     }
 
     private static List<Person> findPeople(List<Person> phonebook, Scanner scanner) {
@@ -148,5 +117,21 @@ public class Main {
                 .collect(Collectors.toList());
 
     }
+
+    private static List<Person> searchByAgeGreaterThan(List<Person> phonebook, int age) {
+        List<Person> filteredList = new ArrayList<>();
+        for (Person p : phonebook) {
+            if (p.age >= age) {
+                filteredList.add(p);
+            }
+        }
+        return filteredList;
+    }
+
 }
+
+
+
+
+
 
